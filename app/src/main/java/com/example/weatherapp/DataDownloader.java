fa975032af825fd56c39322ca6ded40fe210898b
Mikołaj Rajczyk <mikolajrajczyk01@gmail.com>
2023-04-23 17:00:03 +0200

    async data downloading class added, changes to main activity

---
+++ app/src/main/java/com/example/weatherapp/DataDownloader.java
@@ -0,0 +1,49 @@
+package com.example.weatherapp;
+
+import android.content.Context;
+
+import com.example.weatherapp.models.FiveDayResponse;
+import com.example.weatherapp.models.TodayResponse;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class DataDownloader {
+
+    private final OpenWeatherMapService mapService;
+    private final Context context;
+    private final Callback callback;
+
+    DataDownloader(Context context, Callback callback) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(OpenWeatherMapService.API_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        this.mapService = retrofit.create(OpenWeatherMapService.class);
+        this.context = context;
+        this.callback = callback;
+    }
+
+    public void downloadTodaysWeather(String city) {
+        Call<TodayResponse> todayWeather = mapService.getTodayWeather(city, OpenWeatherMapService.API_KEY);
+        try {
+            todayWeather.enqueue(this.callback);
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void downloadForecastWeather(String city) {
+        Call<FiveDayResponse> fiveDayWeather = mapService.getForecastWeather(city, OpenWeatherMapService.API_KEY);
+        try {
+            fiveDayWeather.enqueue(this.callback);
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,33 +1,53 @@
 package com.example.weatherapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.preference.PreferenceManager;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.weatherapp.adapters.ViewPagerAdapter;
-import com.example.weatherapp.models.FiveDayResponse;
-import com.example.weatherapp.models.TodayResponse;
+import com.example.weatherapp.viewmodels.WeatherViewModel;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
 import retrofit2.Call;
+import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Callback {
+
+    private String units;
+    private WeatherViewModel weatherVM;
+    private TextView cityTextView;
+    private DataDownloader dataDownloader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (ContextCompat.checkSelfPermission(MainActivity.this, "android.permission.INTERNET") == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{"android.permission.INTERNET"}, 3);
+        }
+        //check if premission granted, if not, spawn a toast to inform user
+        if(ContextCompat.checkSelfPermission(MainActivity.this, "android.permission.INTERNET") == PackageManager.PERMISSION_DENIED) {
+            Toast.makeText(MainActivity.this, "Could not get network permissions", Toast.LENGTH_SHORT).show();
+        }
+        //setting thread policy for main thread api requests(Testing)
         int SDK_INT = android.os.Build.VERSION.SDK_INT;
         if (SDK_INT > 8)
         {
@@ -36,30 +56,15 @@ public class MainActivity extends AppCompatActivity {
             StrictMode.setThreadPolicy(policy);
         }
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(OpenWeatherMapService.API_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+        this.cityTextView = findViewById(R.id.selectedCity);
+        this.weatherVM = new ViewModelProvider(this).get(WeatherViewModel.class);
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        //read temperature unit from preferences, works cuz we always create main activity when coming back from settings
+        this.units = preferences.getString("Temperature_units","Kelvin");
+        this.dataDownloader = new DataDownloader(getApplicationContext(), this);
 
-        OpenWeatherMapService service = retrofit.create(OpenWeatherMapService.class);
-        Call<TodayResponse> todayWeather = service.getTodayWeather("Łódź", OpenWeatherMapService.API_KEY);
-        Call<FiveDayResponse> fiveDayWeather = service.getForecastWeather("Łódź", OpenWeatherMapService.API_KEY);
-        try {
-            System.out.println("today data");
-            Response<TodayResponse> response = todayWeather.execute();
-            TodayResponse apiResponse = response.body();
-            //API response
-            System.out.println(apiResponse);
-
-            System.out.println("forecast five day data");
-            Response<FiveDayResponse> responseFive = fiveDayWeather.execute();
-            FiveDayResponse apiResponseFive = responseFive.body();
-            //API response
-            System.out.println(apiResponseFive);
-        }
-        catch (Exception ex) {
-            ex.printStackTrace();
-        }
+        this.dataDownloader.downloadTodaysWeather(this.cityTextView.getText().toString());
+        this.dataDownloader.downloadForecastWeather(this.cityTextView.getText().toString());
 
         TabLayout tabLayout = findViewById(R.id.fragmentTabs);
         ViewPager2 viewPager2 = findViewById(R.id.viewPagerFragments);
@@ -103,5 +108,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onResponse(@NonNull Call call, @NonNull Response response) {
+        System.out.println(response.body());
+    }
+
+    @Override
+    public void onFailure(@NonNull Call call, @NonNull Throwable t) {
+        System.out.println("Error!");
     }
 }
